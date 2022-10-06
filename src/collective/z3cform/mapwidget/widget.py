1b3e4ca8761d19689ba286539363c95fbe51cd23
muellers-saw-leipzig <muellers@saw-leipzig.de>
2022-10-06 10:17:52 +0200

    modified code to fit python3/zope

--- src/collective/z3cform/mapwidget/widget.py
+++ src/collective/z3cform/mapwidget/widget.py
@@ -1,6 +1,5 @@
 from zope.component import adapter
-from zope.interface import implementer
-from zope.interface import implementsOnly
+from zope.interface import implementer, implementer_only
 from zope.schema.interfaces import IField
 
 from z3c.form.interfaces import IFormLayer
@@ -12,10 +11,17 @@ from z3c.form.widget import FieldWidget
 from z3c.form.interfaces import DISPLAY_MODE
 
 from collective.geo.mapwidget.browser import widget
+from collective.geo.mapwidget.browser.shapewidget import  ShapeMapWidget
 
 
 class MapWidget(widget.MapWidget):
     js = None
+
+    def __init__(self, view, request, context):
+        self.view = view
+        self.request = request
+        self.context = context
+        #self.mapid = "%s-map" % self.view.name.replace('.', '-')
 
     @property
     def mapid(self):
@@ -33,13 +39,17 @@ class IFormMapWidget(ITextAreaWidget):
     """Interface for z3c.form map widget"""
     pass
 
-
+@implementer_only(IFormMapWidget)
 class FormMapWidget(TextAreaWidget):
-    implementsOnly(IFormMapWidget)
+
+    def update(self):
+        super(FormMapWidget, self).update()
 
     @property
     def cgmap(self):
+        # import pdb; pdb.set_trace()
         if self.mode == DISPLAY_MODE:
+            # return ShapeMapWidget(self, self.request, self.context)
             return MapDisplayWidget(self, self.request, self.context)
         return MapWidget(self, self.request, self.context)
 
